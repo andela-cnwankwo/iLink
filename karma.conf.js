b888f7a181c9943b1f9f185349234b52eb51253c
Ethan <emekanwankwo201@gmail.com>
2018-03-21 17:33:26 +0100

    Update karma config file and travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
 
 
     // Continuous Integration mode
@@ -77,6 +77,13 @@ module.exports = function(config) {
           type: 'json',
           dir: 'build/test/json'
       }]
+  },
+
+  customLaunchers: {
+    ChromeHeadlessNoSandbox: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox']
+    }
   }
   })
 }
