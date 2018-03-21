6655305a31c99e80b8ea9005e2323b93bd5b28b5
Ethan <emekanwankwo201@gmail.com>
2018-03-21 11:47:46 +0100

    Create controller

---
+++ app/controllers/numberGeneration.js
@@ -0,0 +1,20 @@
+/**
+ * savePhoneNumbers
+ * @param {object} data
+ */
+function savePhoneNumbers(data) {
+    //Phone number save method
+}
+
+/**
+ * getPhoneNumbers
+ * @param {object} data
+ */
+function getPhoneNumbers(data) {
+    //Phone number get method
+}
+
+module.exports = {
+    savePhoneNumbers,
+    getPhoneNumbers
+};
