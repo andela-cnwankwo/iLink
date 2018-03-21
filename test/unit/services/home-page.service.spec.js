656aafa1e5094ca4b32f5529682399c4de38ac11
Ethan <emekanwankwo201@gmail.com>
2018-03-21 23:53:19 +0100

    Add home-page service test

---
+++ test/unit/services/home-page.service.spec.js
@@ -0,0 +1,31 @@
+describe('HomePage', () => {
+    let mockBackend, homepageService, location, baseUrl;
+    beforeEach(module('iLink'));
+    beforeEach(inject((_$httpBackend_, _$location_, _HomepageService_) => {
+        mockBackend = _$httpBackend_;
+        location = _$location_;
+        homepageService = _HomepageService_;
+        baseUrl = location.absUrl()+'api';
+    }));
+
+    it('should handle a savePhoneNumbers method', () => {
+        mockBackend.expectPOST(`${baseUrl}/saveNumbers`).respond(200, Object({
+        }));
+        homepageService.savePhoneNumbers('data', ()=> {});
+        mockBackend.flush();
+    });
+
+    it('should handle a loadPhoneNumberIds method', () => {
+        mockBackend.expectGET(`${baseUrl}/getNumberIds`).respond(200, Object({
+        }));
+        homepageService.loadPhoneNumberIds(() => {});
+        mockBackend.flush();
+    });
+
+    it('should handle a getPhoneNumbers method', () => {
+        mockBackend.expectGET(`${baseUrl}/getNumber/1`).respond(200, Object({
+        }));
+        homepageService.getPhoneNumbers(1, () => {});
+        mockBackend.flush();
+    });
+})
